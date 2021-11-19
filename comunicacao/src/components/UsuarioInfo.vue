f6fa97052ecd008ff69be2a1241170b207c770de
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-19 01:07:40 -0300

    brincando com props

---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuario: <strong>{{ nome }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      required: true,
+      //  default: 'default na props'
+    },
+  },
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
