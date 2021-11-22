44e662d2e1dc13d8b4e5e203c9e86f2d0dd56741
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-22 09:46:56 -0300

    config

---
+++ citacoes/src/components/Citacoes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="citacoes">
+    <span>
+      <button @click="numero--">&lt;</button>
+      <button @click="numero++">&gt;</button>
+    </span>
+    {{ citacoes[indice].texto }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero: 0,
+      citacoes: [
+        {
+          fonte: "Frases Motivacionais",
+          texto: "A persistência é o caminho do êxito..",
+          autor: "Charles Chaplin",
+        },
+        {
+          fonte: "Frases de Músicas",
+          texto:
+            "Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação..",
+          autor: "Charlie Brown Jr",
+        },
+        {
+          fonte: "Frases de Pensador",
+          texto:
+            "Você precisa fazer aquilo que pensa que não é capaz de fazer.",
+          autor: "Eleanor Roosevelt",
+        },
+      ],
+    };
+  },
+  computed: {
+    indice() {
+      return Math.abs(this.numero % 3);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.citacoes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
