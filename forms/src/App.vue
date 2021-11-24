494bf1db177993006d3a1f571a768a0ed2b81e79
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-24 20:31:19 -0300

    bind no select

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -47,8 +47,15 @@
           >
         </Rotulo>
         <Rotulo nome="Prioridade">
-          <select name="" id="">
-            <option></option>
+          <select v-model="prioridade">
+            <option
+              v-for="prioridade in prioridades"
+              :key="prioridade.codigo"
+              :value="prioridade.codigo"
+              :selected="prioridade.codigo === 2"
+            >
+              {{ prioridade.codigo }} - {{ prioridade.nome }}
+            </option>
           </select>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
@@ -88,7 +95,7 @@
           <span>{{ produto }}</span>
         </Rotulo>
         <Rotulo nome="Prioridade">
-          <span>???</span>
+          <span>{{ prioridade }}</span>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
           <span>???</span>
@@ -110,6 +117,12 @@ export default {
       mensagem: "",
       caracteristicas: [],
       produto: "web",
+      prioridade: 2,
+      prioridades: [
+        { codigo: 1, nome: "Baixa" },
+        { codigo: 2, nome: "Moderada" },
+        { codigo: 3, nome: "Alta" },
+      ],
       usuario: {
         email: "",
         senha: "",
