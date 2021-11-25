007251e9bb5d849b62a919fb3d6c524ff7032c80
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-25 02:01:17 -0300

    form ok

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Registrar Reclamação</h1>
     <div class="conteudo">
-      <form class="painel">
+      <form class="painel" v-if="!enviado">
         <div class="cabecalho">Formulário</div>
         <Rotulo nome="E-mail">
           <input v-model.lazy.trim="usuario.email" type="text" />
@@ -59,12 +59,12 @@
           </select>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
-          <Escolha />
+          <Escolha v-model="escolha" />
         </Rotulo>
         <hr />
-        <button>Enviar</button>
+        <button @click.prevent="enviar">Enviar</button>
       </form>
-      <div class="painel">
+      <div class="painel" v-else>
         <div class="cabecalho">Resultado</div>
         <Rotulo nome="E-mail">
           <span>{{ usuario.email }}</span>
@@ -98,7 +98,7 @@
           <span>{{ prioridade }}</span>
         </Rotulo>
         <Rotulo nome="Primeira Reclamação?">
-          <span>???</span>
+          <span>{{ escolha }}</span>
         </Rotulo>
       </div>
     </div>
@@ -112,6 +112,11 @@ import Escolha from "./components/Escolha.vue";
 export default {
   name: "app",
   components: { Rotulo, Escolha },
+  methods: {
+    enviar() {
+      this.enviado = true;
+    },
+  },
   data() {
     return {
       mensagem: "",
@@ -128,6 +133,8 @@ export default {
         senha: "",
         idade: 25,
       },
+      escolha: true,
+      enviado: false,
     };
   },
 };
