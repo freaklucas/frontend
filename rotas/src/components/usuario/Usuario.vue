4d073e545549da26f85fd5573eadf5438718da92
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-30 23:42:59 -0300

    consertando mudanças de parametro

--- rotas/src/components/usuario/Usuario.vue
+++ rotas/src/components/usuario/Usuario.vue
@@ -2,12 +2,26 @@
   <div class="usuario">
     <h2>Seção de Usuário</h2>
     <hr />
+    <p>
+      <strong>Código: </strong>
+      {{ id }}
+    </p>
     <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    },
+  },
   methods: {
     irParaInicio() {
       this.$router.push("/");
