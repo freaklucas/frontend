c420d5332489190f4655bc54241726e8a5cdbe7b
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-18 23:59:01 -0300

    config data

--- category/src/store/index.js
+++ category/src/store/index.js
@@ -1,12 +1,17 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    characteres: [],
+    charactersFilter: [],
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    setCharacters(state, payload) {
+      state.characters = payload;
+    },
+    setCharacters(state, payload) {
+      state.charactersFilter = payload;
+    },
+  actions: {},
+  modules: {},
+});
