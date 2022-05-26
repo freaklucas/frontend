7cf73eeacb3c70bf8e2589b56d46f934d35be7c0
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-26 20:32:42 -0300

    style search

---
+++ category/src/components/FilterName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="pesquise pelo nome" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.search {
+  width: 400px;
+  margin: 3rem auto 0;
+
+  input {
+    height: 54px;
+    width: 400px;
+    border: none;
+    border-radius: 10px;
+    padding: 0 0.5rem;
+  }
+}
+</style>
